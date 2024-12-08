23899faf5603ba56ce454e20abf07be7eae51692
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2024-12-09 01:05:15 +0200

    Day 8 part 2

--- 08.go
+++ 08.go
@@ -92,9 +92,96 @@ func advent08_1() {
 }
 
 func advent08_2() {
+	bytes, _ := os.ReadFile("08.txt")
+	data := strings.Split(string(bytes), "\n")
+	lines := data[:len(data)-1]
+	grid := make([][]Location, len(lines))
+	for i, line := range lines {
+		grid[i] = make([]Location, len(line))
+		for j, c := range line {
+			grid[i][j] = Location{j, i, c, '.'}
+		}
+	}
 
+	for i, line := range grid {
+		for j, loc := range line {
+			if loc.antenna != '.' {
+				// Look ahead for all other antennae of this type
+				// Calculate and mark their antinodes
+				same_antennas := []Location{}
+				// Scan line forward
+				for _, pos := range line[j+1:] {
+					if pos.antenna == loc.antenna {
+						same_antennas = append(same_antennas, pos)
+					}
+				}
+				// Scan rest of grid
+				for _, line := range grid[i+1:] {
+					for _, pos := range line {
+						if pos.antenna == loc.antenna {
+							same_antennas = append(same_antennas, pos)
+						}
+					}
+				}
+				for _, other := range same_antennas {
+					dx := other.x - loc.x
+					dy := other.y - loc.y
+
+					nx := loc.x
+					ny := loc.y
+					for {
+						if nx >= 0 && nx < len(grid[0]) &&
+							ny >= 0 && ny < len(grid) {
+							res := grid[ny][nx]
+							grid[ny][nx] = Location{
+								res.x,
+								res.y,
+								res.antenna,
+								loc.antenna,
+							}
+							nx -= dx
+							ny -= dy
+						} else {
+							break
+						}
+					}
+
+					mx := other.x
+					my := other.y
+					for {
+						if mx >= 0 && mx < len(grid[0]) &&
+							my >= 0 && my < len(grid) {
+							res := grid[my][mx]
+							grid[my][mx] = Location{
+								res.x,
+								res.y,
+								res.antenna,
+								loc.antenna,
+							}
+							mx += dx
+							my += dy
+						} else {
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Sum the number of antinodes on map
+	sum := 0
+	for _, line := range grid {
+		for _, loc := range line {
+			if loc.antinode != '.' {
+				sum += 1
+			}
+		}
+	}
+	fmt.Println(sum)
 }
 
 func main() {
 	advent08_1()
+	advent08_2()
 }
